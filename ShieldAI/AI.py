2ccb7220a6ae024d9b298ed22b81d497f2996304
Hyperion454 <connect.hyperion@proton.me>
2024-11-23 13:11:39 +0300

    Rapor Eklendi

--- ShieldAI/AI.py
+++ ShieldAI/AI.py
@@ -1,10 +1,13 @@
 import json
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from nltk.chat.util import Chat, reflections
 
 nltk.download("punkt")
 nltk.download("punkt_tab")
+nltk.download("stopwords")
+
 
 # jsondan veri alma
 def get_info(konu):
@@ -16,9 +19,19 @@ def get_info(konu):
         return "Veri dosyası bulunamadı."
     
 
-print("Yapay zeka ile sohbetinize son vermek için 'kapat' yazabilirsiniz.")
+print("Shield AI: Sohbetinize son vermek için 'kapat' yazabilirsiniz.")
 while True:
     user_input = input("Siz: ")
-    tokenized = word_tokenize(user_input)
-    print(tokenized)
-
+    if user_input.lower() == "kapat":
+        print("Shield AI: Görüşmek üzere.")
+        break
+    else:
+        tokenized = word_tokenize(user_input)
+        clear_text = set(tokenized) - set(stopwords.words("turkish"))
+        for word in clear_text:
+            if word.lower() == (r"merhaba|naber|nasılsın|sen nesin|nesin sen|selam|merhabalar|selamlar|adın|ismin|ismini|adını|meraba|merabalar"):
+                response = get_info("merhaba")
+                print(f"Shield AI: {response}")
+            elif word.lower() == (r"şifre|şifrem|şifremi|şifrelerimi|parola|parolalarda|parolalar|şifreler|parolalarımı|parolam|parolamı"):
+                response = get_info("şifre güvenliği")
+                print(f"Shield AI: {response}")
