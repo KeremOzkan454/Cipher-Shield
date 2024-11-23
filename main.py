d703e9ed96c15f7f5f51becea710b6dd437b59b5
Hyperion454 <connect.hyperion@proton.me>
2024-11-23 17:59:54 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,161 +1,94 @@
-import tkinter as tk
-from tkinter import messagebox
-import re
-import random
-import string
+import module
 
-
-
-def pass_cont(passwd):
-    uzunluk = len(passwd) >= 12
-    kucukh = bool(re.search(r"[a-z]", passwd))
-    buyukh = bool(re.search(r"[A-Z]", passwd))
-    sayi = bool(re.search(r"[0-9]", passwd))
-    karakter = bool(re.search(r"[!@#$%^&*()_+]", passwd))
-    return uzunluk, kucukh, buyukh, sayi, karakter
-
-def check(passwd):
-    pwned = False
-
-    with open("pwned.txt", "r", encoding="ISO-8859-1") as dosya:
-        for satır in dosya:
-            if satır.strip() == str(passwd):
-                pwned = True
-                break
-
-    return pwned
-
-def check_pass_str():
-    parola = entry.get()
-    if not parola:
-        messagebox.showwarning("Uyarı", "Lütfen bir parola girin!!!")
-        return
-
-    uzunluk, kucukh, buyukh, sayi, karakter = pass_cont(parola)
-    uzu = len(parola)
-    deger = 0
-
-    if uzunluk:
-        deger += 2
-    if kucukh:
-        deger += 1
-    if buyukh:
-        deger += 2
-    if sayi:
-        deger += 2
-    if karakter:
-        deger += 3
-
-    result_text = f"Parolanızın güvenlik düzeyi: {deger} / 10\n"
-
-    if deger == 0:
-        result_text += "[_ _ _ _ _ _ _ _ _ _]"
-    elif deger == 1:
-        result_text += "[# _ _ _ _ _ _ _ _ _]"
-    elif deger == 2:
-        result_text += "[# # _ _ _ _ _ _ _ _]"
-    elif deger == 3:
-        result_text += "[# # # _ _ _ _ _ _ _]"
-    elif deger == 4:
-        result_text += "[# # # # _ _ _ _ _ _]"
-    elif deger == 5:
-        result_text += "[# # # # # _ _ _ _ _]"
-    elif deger == 6:
-        result_text += "[# # # # # # _ _ _ _]"
-    elif deger == 7:
-        result_text += "[# # # # # # # _ _ _]"
-    elif deger == 8:
-        result_text += "[# # # # # # # # _ _]"
-    elif deger == 9:
-        result_text += "[# # # # # # # # # _]"
-    elif deger == 10:
-        result_text += "[# # # # # # # # # #]"
-
-    if uzunluk and kucukh and buyukh and sayi and karakter:
-        result_text += "\nYaşasın! Parolanız oldukça güvenli."
-    elif deger > 4 and deger < 10:
-        result_text += "\nParolanız kısmen güvenli. Lütfen aşağıdaki önerilere dikkat ederek farklı bir parola belirleyiniz!"
-    elif deger <= 4:
-        result_text += "\nParolanız çok zayıf. Lütfen aşağıdaki önerilere dikkat ederek farklı bir parola belirleyiniz!"
+while True:
+    print("Parolanızın güvenliğini kontrol etmek için 1")
+    print("Parolanızın veri ihlallerinde açığa çıktığını sorulamak için 2")
+    print("SGüçlü parola önerisi için 3")
+    user_input = input("Uygulamadan çıkış yapmak için 4 tuşlayınız\n>>>")
     
-    if not uzunluk:
-        result_text += f"\nParolanız en az 12 karakter uzunluğunda olmalıdır! Sizin parolanız ise {uzu} uzunluğunda."
-    if not kucukh:
-        result_text += "\nParolanız küçük harf içermelidir!"
-    if not buyukh:
-        result_text += "\nParolanız büyük harf içermelidir!"
-    if not sayi:
-        result_text += "\nParolanız sayı içermelidir!"
-    if not karakter:
-        result_text += "\nParolanız özel karakter içermelidir!"
+    # Parola Güvenlik kontrolü
+    if user_input == "1":
 
-    result.delete(1.0, tk.END)
-    result.insert(tk.END, result_text)
+        parola = input("Parolanızı giriniz >>> ")
+        uzunluk, kucuk_harf, buyuk_harf, sayi, karakter = module.password_control(parola)
+        parola_uzunlugu = len(parola)
+        deger = 0
 
-def check_pwned_pass():
-    parola = entry.get()
-    if not parola:
-        messagebox.showwarning("Uyarı", "Lütfen bir parola girin!")
-        return
+        if uzunluk:
+            deger += 2
+        if kucuk_harf:
+            deger += 1
+        if buyuk_harf:
+            deger += 2
+        if sayi:
+            deger += 2
+        if karakter:
+            deger += 3
 
-    pwn = check(parola)
-    result_text = ""
+        print(f"Parolanızın güvenlik düzeyi: {deger} / 10\n")
 
-    if pwn:
-        result_text = "\nEyvah! Parolanız daha önceki veri ihlallerinde açığa çıkarılmış. Derhal bu parolayı değiştirmeniz önerilir!"
+        if deger == 0:
+            print("[_ _ _ _ _ _ _ _ _ _]")
+        elif deger == 1:
+            print("[# _ _ _ _ _ _ _ _ _]")
+        elif deger == 2:
+            print("[# # _ _ _ _ _ _ _ _]")
+        elif deger == 3:
+            print("[# # # _ _ _ _ _ _ _]")
+        elif deger == 4:
+            print("[# # # # _ _ _ _ _ _]")
+        elif deger == 5:
+            print("[# # # # # _ _ _ _ _]")
+        elif deger == 6:
+            print("[# # # # # # _ _ _ _]")
+        elif deger == 7:
+            print("[# # # # # # # _ _ _]")
+        elif deger == 8:
+            print("[# # # # # # # # _ _]")
+        elif deger == 9:
+            print("[# # # # # # # # # _]")
+        elif deger == 10:
+            print("[# # # # # # # # # #]")
+
+        if deger > 9:
+            print("\nYaşasın! Parolanız oldukça güvenli.")
+        elif deger > 4 and deger < 10:
+            print("\nParolanız kısmen güvenli. Lütfen aşağıdaki önerilere dikkat ederek farklı bir parola belirleyiniz!")
+        elif deger <= 4:
+            print("\nParolanız çok zayıf. Lütfen aşağıdaki önerilere dikkat ederek farklı bir parola belirleyiniz!")
+        
+        if not uzunluk:
+            print(f"\nParolanız en az 12 karakter uzunluğunda olmalıdır! Sizin parolanız ise {parola_uzunlugu} uzunluğunda.")
+        if not kucuk_harf:
+            print("\nParolanız küçük harf içermelidir!")
+        if not buyuk_harf:
+            print("\nParolanız büyük harf içermelidir!")
+        if not sayi:
+            print("\nParolanız sayı içermelidir!")
+        if not karakter:
+            print("\nParolanız özel karakter içermelidir!")
+
+    # Parola pwned kontrolü
+    elif user_input == "2":
+        parola = input("Parolanızı giriniz >>> ")
+        is_pwn = module.check(parola)
+
+        if not is_pwn:
+            print("Parolanız şu ana kadarki hiçbir veri ihlalinde açığa çıkmamış. Parolanız güvende.")
+        else:
+            print("Eyvah! Görünüşe göre parolanız bir veri ihlalinde ortaya çıkarılmış. Bu parolayı değiştirmeniz şiddetle tavsiye edilir!")
+
+    # Parola önerisi
+    elif user_input == "3":
+        oneri = module.generate_password()
+        print(f"Güçlü parola: {oneri}")
+
+    # Çıkış   
+    elif user_input == "4":
+        print("Tekrar görüşmek dileğiyle...")
+        break
+
+    # Hatalı sayı girişi 
     else:
-        result_text = "\nParolanız şu ana kadarki hiçbir veri ihlalinde açığa çıkmamış. Parolanız güvende."
+        print("Lütfen geçerli bir sayı giriniz!!!")
 
-    result.delete(1.0, tk.END)
-    result.insert(tk.END, result_text)
-
-def generate_pass(leng=16):
-    chars = string.ascii_letters
-    chars += string.digits
-    chars += string.punctuation
-    
-    passwd = ""
-    result_text = ""
-    for i in range(leng):
-        passwd += random.choice(chars)
-        
-    result_text = passwd
-    
-    result.delete(1.0, tk.END)
-    result.insert(tk.END, result_text)
-        
-        
-
-def quit_prog():
-    window.quit()
-
-
-window = tk.Tk()
-window.title("Cipher Shield")
-window.geometry("600x700")
-window.config(bg="#E1EFFF")
-
-# Output text
-result = tk.Text(window, height=15, width=40, wrap=tk.WORD, font=("Arial", 15), bg="#D1D9FF")
-result.pack(pady=10)
-
-# Kullanıcı giriş
-entry_label = tk.Label(window, text="Parolanızı giriniz:", font=("Arial", 16), bg="#E1EFFF")
-entry_label.pack(pady=5)
-entry = tk.Entry(window, width=30, font=("Arial", 12))
-entry.pack(pady=5)
-
-# Butonlar
-buton_check_pass = tk.Button(window, text="Parola Güvenliği Testi", width=30, font=("Arial", 12), bg="#4C79FF", command=check_pass_str)
-buton_check_pass.pack(pady=5)
-
-buton_check_pwned = tk.Button(window, text="Parola Pwned Kontrolü", width=30, font=("Arial", 12), bg="#4C79FF", command=check_pwned_pass)
-buton_check_pwned.pack(pady=5)
-
-buton_oner_pass = tk.Button(window, text="Parola Öner", width=30, font=("Arial",12),bg="#4C79FF",command=generate_pass)
-buton_oner_pass.pack(pady=5)
-
-buton_exit = tk.Button(window, text="Çıkış", width=30, font=("Arial", 12), bg="#FF4C4C", command=quit_prog)
-buton_exit.pack(pady=5)
-
-window.mainloop()
---
+++ module.py
@@ -0,0 +1,35 @@
+import re
+import random
+import string
+
+
+def password_control(parola):
+    uzunluk = len(parola) >= 12
+    kucuk_harf = bool(re.search(r"[a-z]", parola))
+    buyuk_harf = bool(re.search(r"[A-Z]", parola))
+    sayi = bool(re.search(r"[0-9]", parola))
+    karakter = bool(re.search(r"[!@#$%^&*()_+]", parola))
+
+    return uzunluk, kucuk_harf, buyuk_harf, sayi, karakter
+
+def check(parola):
+    pwned = False
+
+    with open("pwned.txt", "r", encoding="ISO-8859-1") as dosya:
+        for satır in dosya:
+            if satır.strip() == str(parola):
+                pwned = True
+                break
+
+    return pwned
+
+def generate_password(leng=16):
+    chars = string.ascii_letters
+    chars += string.digits
+    chars += string.punctuation
+    
+    password = ""
+    for i in range(leng):
+        password += random.choice(chars)
+        
+    return password
