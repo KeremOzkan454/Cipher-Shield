113c639549869e3e685e22e349877df8ba30ba37
Hyperion454 <connect.hyperion@proton.me>
2024-11-23 12:46:13 +0300

    Updated AI

--- ShieldAI/AI.py
+++ ShieldAI/AI.py
@@ -1,5 +1,10 @@
 import json
+import nltk
+from nltk.tokenize import word_tokenize
 from nltk.chat.util import Chat, reflections
+
+nltk.download("punkt")
+nltk.download("punkt_tab")
 
 # jsondan veri alma
 def get_info(konu):
@@ -10,37 +15,10 @@ def get_info(konu):
     except FileNotFoundError:
         return "Veri dosyası bulunamadı."
     
-x1 = get_info("Şifre Güvenliği")
-x2 = get_info("Veri İhlali")
-x3 = get_info("Çift Faktörlü Doğrulama")
-x4 = get_info("Merhaba")
 
-
-
-# cevap vereceği zımbırtılar
-patterns = [
-    (r'^(şifre güvenliği|güçlü şifre|şifre|şifremi nasıl korurum)$',x1),
-
-    (r'^(veri ihlali|veri güvenliği|hesap güvenliği)$',x2),
-
-    (r'^(çift faktörlü doğrulama|2fa|2 faktörlü doğrulama|iki faktör doğrulama|iki faktörlü doğrulama)$',x3),
-
-    (r'^(merhaba|selam|merhabalar|nesin sen|sen nesin|adın ne|selamlar|naber|nabersin)$',x4),
-
-    (r'(.*)$',"Üzgünüm, bu konuda bilgi veremiyorum. Başka bir konu hakkında sorunuz varsa, size yardımcı olabilirim.")
-]
-
-# Chatbot oluşturma
-chatbot = Chat(patterns, reflections)
-
-#### Ana döngü ####
-
-print("Chatbot'a 'kapat' yazana kadar sohbet edebilirsiniz.")
+print("Yapay zeka ile sohbetinize son vermek için 'kapat' yazabilirsiniz.")
 while True:
     user_input = input("Siz: ")
-    if user_input.lower() == 'kapat':
-        print("Chatbot: Görüşmek üzere!")
-        break
-    else:
-        outpt = chatbot.respond(user_input)
-        print(f"Chatbot: {outpt}")
+    tokenized = word_tokenize(user_input)
+    print(tokenized)
+
